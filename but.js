3fdb0efaef947e91815969e76d60bec9989fbaab
Emanuel <emanuel.casellero@gmail.com>
2023-06-09 00:44:39 -0300

    lis

--- but.js
+++ but.js
@@ -3,5 +3,6 @@ let nav = document.getElementById("nav");
 let close = document.getElementById("close")
 
 menu.addEventListener("click",()=>{nav.classList.add("menu");});
-
-close.addEventListener("click",()=>{nav.classList.remove("menu")})
+menu.addEventListener("click",()=>{menu.classList.add("delf");});
+close.addEventListener("click",()=>{nav.classList.remove("menu")});
+close.addEventListener("click",()=>{menu.classList.remove("delf")});
