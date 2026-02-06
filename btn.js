f05dc0b1ebb1b3eba07b31032ea3d5c2636f295a
Emanuel <emanuel.casellero@gmail.com>
2026-02-06 13:46:13 -0300

    portafolio2.0

--- btn.js
+++ btn.js
@@ -6,5 +6,6 @@ let cdss = document.getElementById("cdss");
 function quit(){btnoldh.classList.add("mm");
  btnoldh.classList.remove("alert")  ;
  cds.classList.remove("opacity");}
- function addd(){btnoldh.classList.toggle("alert");  btnoldh.classList.remove("mm") ;cds.classList.toggle("opacity");}
- function quitt(){btnoldhh.classList.add("mm"); btnoldhh.classList.remove("alert")  ;cdss.classList.remove("opacity");}
+ function addd(){btnoldh.classList.toggle("alert");btnoldh.classList.remove("mm");cds.classList.toggle("opacity");}
+ function quitt(){btnoldhh.classList.add("mm"); btnoldhh.classList.remove("alert");cdss.classList.remove("opacity");}
+ cds.addEventListener("click", ()=>{quit()})
